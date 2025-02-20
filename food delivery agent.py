6d31948846be6a3f3c0e8c3d8e499b8b9e24e836
PSewmuthu <apsewmuthu@gmail.com>
2025-02-20 15:07:11 +0530

    Add show_menu() for show the food menu

--- food delivery agent.py
+++ food delivery agent.py
@@ -9,3 +9,20 @@ class FoodDeliveryAgent:
         self.order = []
         self.total = 0
         self.address = ''
+
+    def show_menu(self):
+        '''
+        Agent: Welcome to Food Delivery! Here are the items we have: 
+        1. Pizza - $8 
+        2. Burger - $5 
+        3. Pasta - $7 
+        4. Salad - $4 
+        Please type the item(s) you want to order (separate with commas if more than one).
+        '''
+
+        print("\nAgent: Welcome to Food Delivery! Here are the items we have:")
+
+        for num, (itm, amnt) in enumerate(self.menu.items(), 1):
+            print(f"{num}. {itm}\t - \t${amnt}")
+
+        print("Please type the item(s) you want to order (separate with commas if more than one).\n")
